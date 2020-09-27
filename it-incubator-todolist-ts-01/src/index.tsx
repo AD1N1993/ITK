d2aec4119d295a08db4d8b4a34c9501bcfa77c67
YuryGrivickiy <griv1993@gmail.com>
2020-09-27 22:50:17 +0300

    lesson 14 Refactoring

--- it-incubator-todolist-ts-01/src/index.tsx
+++ it-incubator-todolist-ts-01/src/index.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import AppWithRedux from "./AppWithRedux";
-import {Provider} from "react-redux";
-import {store} from "./state/store";
+import App from './App/App';
+import {store} from './App/store';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
     <Provider store={store}>
-        <AppWithRedux/>
+        <App/>
     </Provider>, document.getElementById('root'));
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- it-incubator-todolist-ts-01/src/stories/decorators/ReduxStoreProviderDecorator.tsx
+++ it-incubator-todolist-ts-01/src/stories/decorators/ReduxStoreProviderDecorator.tsx
@@ -1,46 +1,41 @@
 import React from 'react'
 import {Provider} from 'react-redux'
 import {combineReducers, createStore} from 'redux'
+import {tasksReducer} from '../../Features/Todolists/Todolist/tasks-reducer'
+import {todolistsReducer} from '../../Features/Todolists/Todolist/todolists-reducer'
 import {v1} from 'uuid'
-import {tasksReducer} from "../../state/tasks-reducer";
-import {todolistsReducer} from "../../state/todolists-reducer";
-import {AppRootStateType} from "../../state/store";
-import {TaskStatuses, TodoTaskPriorities} from "../../api/todolist-api";
+import {AppRootStateType} from '../../App/store'
+import {TaskPriorities, TaskStatuses} from '../../api/todolists-api'
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
 
-const initialGlobalState = {
+const initialGlobalState: AppRootStateType = {
     todolists: [
-        {id: "todolistId1", title: "What to learn?", filter: "all", addedDate:"", order:0},
-        {id: "todolistId2", title: "What to Buy?", filter: "active",addedDate:"", order:1},
+        {id: "todolistId1", title: "What to learn", filter: "all", addedDate: '', order: 0},
+        {id: "todolistId2", title: "What to buy", filter: "all", addedDate: '', order: 0}
     ] ,
     tasks: {
         ["todolistId1"]: [
-            {id: v1(), title: 'HTML', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId1" },
-            {id: v1(), title: 'CSS', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId1" },
-            {id: v1(), title: 'JS', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId1" },
+            {id: v1(), title: "HTML&CSS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "JS", status: TaskStatuses.Completed, todoListId: "todolistId1", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
         ],
         ["todolistId2"]: [
-            {id: v1(), title: 'React', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId2" },
-            {id: v1(), title: 'Angular', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId2" },
-            {id: v1(), title: 'Vue', status: TaskStatuses.Completed,addedDate:"",deadline:"", description:"",
-                priority: TodoTaskPriorities.Low, order:2, startDate:"",  todoListId: "todolistId2" },
+            {id: v1(), title: "Milk", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low},
+            {id: v1(), title: "React Book", status: TaskStatuses.Completed, todoListId: "todolistId2", description: '',
+                startDate: '', deadline: '', addedDate: '', order: 0, priority: TaskPriorities.Low}
         ]
     }
 };
 
-export const storyBookStore = createStore(rootReducer, initialGlobalState as AppRootStateType);
+export const storyBookStore = createStore(rootReducer, initialGlobalState);
 
 export const ReduxStoreProviderDecorator = (storyFn: any) => (
     <Provider
         store={storyBookStore}>{storyFn()}
     </Provider>)
-
