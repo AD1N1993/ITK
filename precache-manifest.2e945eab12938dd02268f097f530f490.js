53db58b53ee1d4619b8307350df0e44aa22f232c
YuryGrivickiy <griv1993@gmail.com>
2020-11-08 11:23:26 +0300

    Updates

---
+++ precache-manifest.2e945eab12938dd02268f097f530f490.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9d0aeabd2d51bf1eefb71fa27e8f4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45b870cfe9217562d655",
+    "url": "/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "77b5fde0e970075e1fe4",
+    "url": "/static/js/2.06aea117.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.06aea117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b870cfe9217562d655",
+    "url": "/static/js/main.1cbbe5a9.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
